cdf7d64489fabc880797740327e2bb881b834ffb
unknown <devopuser@INMDECVD01.suramericana.com.co>
2016-09-02 14:02:28 -0500

    version 1

---
+++ SuraTestReport/SuraTestReport/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuraTestReport.MvcApplication" Language="C#" %>
---
+++ SuraTestReport/SuraTestReport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuraTestReport.Startup))]
+namespace SuraTestReport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
